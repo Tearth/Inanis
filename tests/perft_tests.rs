319e8bef4a3307924ba331cc5cf3ab336421361f
Tearth <tearthdev@gmail.com>
2021-07-18 13:28:41 +0200

    Perft tests

---
+++ tests/perft_tests.rs
@@ -0,0 +1,32 @@
+#[cfg(test)]
+mod perft_tests {
+    use ina::{movegen, patterns, perft};
+    use std::sync::Once;
+
+    static INIT: Once = Once::new();
+
+    macro_rules! perft_tests {
+        ($($name:ident: $depth:expr, $expected_leafs_count:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    INIT.call_once(|| {
+                        patterns::init();
+                        movegen::init();
+                    });
+
+                    assert_eq!($expected_leafs_count, perft::run($depth));
+                }
+            )*
+        }
+    }
+
+    perft_tests! {
+        perft_depth_1: 1, 20,
+        perft_depth_2: 2, 400,
+        perft_depth_3: 3, 8902,
+        perft_depth_4: 4, 197281,
+        perft_depth_5: 5, 4865609,
+        perft_depth_6: 6, 119060324,
+    }
+}
