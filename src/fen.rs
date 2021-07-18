78229bffeedb4ae24592d4af310a6daf4779b4c5
Tearth <tearthdev@gmail.com>
2021-07-18 12:19:21 +0200

    Add FEN exporter

--- src/fen.rs
+++ src/fen.rs
@@ -1,4 +1,4 @@
-use crate::{board::*, common::*};
+use crate::{board::*, common::*, helpers::bit_scan};
 
 pub fn fen_to_board(fen: &str) -> Result<Bitboard, &str> {
     let parts: Vec<&str> = fen.split(' ').collect();
@@ -17,8 +17,18 @@ pub fn fen_to_board(fen: &str) -> Result<Bitboard, &str> {
     Ok(board)
 }
 
-pub fn board_to_fen(board: &Bitboard) -> Result<String, &str> {
-    Err("Not implemented")
+pub fn board_to_fen(board: &Bitboard) -> String {
+    let pieces = pieces_to_fen(&board);
+    let active_color = active_color_to_fen(&board);
+    let castling = castling_to_fen(&board);
+    let en_passant = en_passant_to_fen(&board);
+    let halfmove_clock = halfmove_clock_to_fen(&board);
+    let fullmove_number = fullmove_number_to_fen(&board);
+
+    format!(
+        "{} {} {} {} {} {}",
+        pieces, active_color, castling, en_passant, halfmove_clock, fullmove_number
+    )
 }
 
 fn fen_to_pieces<'a>(board: &mut Bitboard, pieces: &str) -> Result<(), &'a str> {
@@ -48,11 +58,65 @@ fn fen_to_pieces<'a>(board: &mut Bitboard, pieces: &str) -> Result<(), &'a str> 
     Ok(())
 }
 
+fn pieces_to_fen(board: &Bitboard) -> String {
+    let mut result = String::new();
+    let mut fields_without_piece = 0;
+
+    for field_index in (0..64).rev() {
+        let piece = board.get_piece(field_index);
+        if piece == u8::MAX {
+            fields_without_piece += 1;
+        } else {
+            if fields_without_piece != 0 {
+                result.push(char::from_digit(fields_without_piece, 10).unwrap());
+                fields_without_piece = 0;
+            }
+
+            let mut piece_symbol = match piece {
+                PAWN => 'P',
+                KNIGHT => 'N',
+                BISHOP => 'B',
+                ROOK => 'R',
+                QUEEN => 'Q',
+                KING => 'K',
+                _ => panic!("Invalid value: piece={}", piece),
+            };
+
+            if (board.pieces[WHITE as usize][piece as usize] & (1u64 << field_index)) == 0 {
+                piece_symbol = (piece_symbol.to_lowercase().collect::<Vec<char>>())[0];
+            }
+
+            result.push(piece_symbol);
+        }
+
+        if (field_index % 8) == 0 {
+            if fields_without_piece != 0 {
+                result.push(char::from_digit(fields_without_piece, 10).unwrap());
+                fields_without_piece = 0;
+            }
+
+            if field_index != 0 {
+                result.push('/');
+            }
+        }
+    }
+
+    result
+}
+
 fn fen_to_active_color<'a>(board: &mut Bitboard, active_color: &str) -> Result<(), &'a str> {
     let color_char = active_color.chars().next().unwrap();
     board.color_to_move = if color_char == 'w' { WHITE } else { BLACK };
 
     Ok(())
+}
+
+fn active_color_to_fen(board: &Bitboard) -> String {
+    match board.color_to_move {
+        WHITE => "w".to_string(),
+        BLACK => "b".to_string(),
+        _ => panic!("Invalid value: board.color_to_move={}", board.color_to_move),
+    }
 }
 
 fn fen_to_castling<'a>(board: &mut Bitboard, castling: &str) -> Result<(), &'a str> {
@@ -73,6 +137,32 @@ fn fen_to_castling<'a>(board: &mut Bitboard, castling: &str) -> Result<(), &'a s
     Ok(())
 }
 
+fn castling_to_fen(board: &Bitboard) -> String {
+    if board.castling_rights == CastlingRights::NONE {
+        return "-".to_string();
+    }
+
+    let mut result = String::new();
+
+    if board.castling_rights.contains(CastlingRights::WHITE_SHORT_CASTLING) {
+        result.push('K');
+    }
+
+    if board.castling_rights.contains(CastlingRights::WHITE_LONG_CASTLING) {
+        result.push('Q');
+    }
+
+    if board.castling_rights.contains(CastlingRights::BLACK_SHORT_CASTLING) {
+        result.push('k');
+    }
+
+    if board.castling_rights.contains(CastlingRights::BLACK_LONG_CASTLING) {
+        result.push('q');
+    }
+
+    result
+}
+
 fn fen_to_en_passant<'a>(board: &mut Bitboard, en_passant: &str) -> Result<(), &'a str> {
     if en_passant == "-" {
         return Ok(());
@@ -85,6 +175,19 @@ fn fen_to_en_passant<'a>(board: &mut Bitboard, en_passant: &str) -> Result<(), &
     Ok(())
 }
 
+fn en_passant_to_fen(board: &Bitboard) -> String {
+    if board.en_passant == 0 {
+        return "-".to_string();
+    }
+
+    let field_index = bit_scan(board.en_passant);
+    let file = field_index % 8;
+    let rank = field_index / 8;
+
+    let result = vec![char::from(b'a' + (7 - file)), char::from(b'1' + rank)];
+    result.into_iter().collect()
+}
+
 fn fen_to_halfmove_clock<'a>(board: &mut Bitboard, halfmove_clock: &str) -> Result<(), &'a str> {
     board.halfmove_clock = match halfmove_clock.parse::<u32>() {
         Ok(value) => value,
@@ -94,6 +197,10 @@ fn fen_to_halfmove_clock<'a>(board: &mut Bitboard, halfmove_clock: &str) -> Resu
     Ok(())
 }
 
+fn halfmove_clock_to_fen(board: &Bitboard) -> String {
+    board.halfmove_clock.to_string()
+}
+
 fn fen_to_fullmove_number<'a>(board: &mut Bitboard, fullmove_number: &str) -> Result<(), &'a str> {
     board.fullmove_number = match fullmove_number.parse::<u32>() {
         Ok(value) => value,
@@ -101,4 +208,8 @@ fn fen_to_fullmove_number<'a>(board: &mut Bitboard, fullmove_number: &str) -> Re
     };
 
     Ok(())
+}
+
+fn fullmove_number_to_fen(board: &Bitboard) -> String {
+    board.fullmove_number.to_string()
 }
