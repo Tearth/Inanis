b43096fe5c9551b3b19ee81938da3097f504c8be
Tearth <tearthdev@gmail.com>
2021-11-01 21:25:23 +0100

    Optimize recalculation of material values

--- src/evaluation/material.rs
+++ src/evaluation/material.rs
@@ -14,11 +14,7 @@ pub fn recalculate_incremental_values(board: &mut Bitboard) {
     for color_index in 0..2 {
         let mut score = 0;
         for piece_index in 0..6 {
-            let mut pieces = board.pieces[color_index][piece_index];
-            while pieces != 0 {
-                pieces = pop_lsb(pieces);
-                score += unsafe { PIECE_VALUE[piece_index] };
-            }
+            score += (bit_count(board.pieces[color_index][piece_index]) as i16) * unsafe { PIECE_VALUE[piece_index] };
         }
 
         board.material_scores[color_index] = score;
