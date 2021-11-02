57f8ba7f245883922d67665d54ad9257b3069af2
Tearth <tearthdev@gmail.com>
2021-11-02 22:28:04 +0100

    Evaluation adjustment

--- src/evaluation/parameters.rs
+++ src/evaluation/parameters.rs
@@ -1,30 +1,30 @@
 // ------------------------------------ //
-// Generated at 2021-11-01 16:09:19 UTC //
+// Generated at 2021-11-02 20:29:37 UTC //
 // ------------------------------------ //
 
-pub static mut PIECE_VALUE: [i16; 6] = [100, 300, 330, 500, 900, 10000];
+pub static mut PIECE_VALUE: [i16; 6] = [92, 332, 375, 561, 1155, 10000];
 
 pub static mut MOBILITY_OPENING: i16 = 2;
-pub static mut MOBILITY_ENDING: i16 = 0;
+pub static mut MOBILITY_ENDING: i16 = 3;
 
-pub static mut DOUBLED_PAWN_OPENING: i16 = -20;
-pub static mut DOUBLED_PAWN_ENDING: i16 = -10;
+pub static mut DOUBLED_PAWN_OPENING: i16 = 1;
+pub static mut DOUBLED_PAWN_ENDING: i16 = -9;
 
-pub static mut ISOLATED_PAWN_OPENING: i16 = -30;
-pub static mut ISOLATED_PAWN_ENDING: i16 = -5;
+pub static mut ISOLATED_PAWN_OPENING: i16 = -22;
+pub static mut ISOLATED_PAWN_ENDING: i16 = 1;
 
-pub static mut CHAINED_PAWN_OPENING: i16 = 5;
-pub static mut CHAINED_PAWN_ENDING: i16 = 0;
+pub static mut CHAINED_PAWN_OPENING: i16 = 4;
+pub static mut CHAINED_PAWN_ENDING: i16 = 12;
 
-pub static mut PASSING_PAWN_OPENING: i16 = 20;
-pub static mut PASSING_PAWN_ENDING: i16 = 60;
+pub static mut PASSING_PAWN_OPENING: i16 = -7;
+pub static mut PASSING_PAWN_ENDING: i16 = 62;
 
 pub static mut PAWN_SHIELD_OPENING: i16 = 10;
-pub static mut PAWN_SHIELD_ENDING: i16 = 0;
+pub static mut PAWN_SHIELD_ENDING: i16 = 7;
 
-pub static mut PAWN_SHIELD_OPEN_FILE_OPENING: i16 = -15;
-pub static mut PAWN_SHIELD_OPEN_FILE_ENDING: i16 = 0;
+pub static mut PAWN_SHIELD_OPEN_FILE_OPENING: i16 = -21;
+pub static mut PAWN_SHIELD_OPEN_FILE_ENDING: i16 = 4;
 
-pub static mut KING_ATTACKED_FIELDS_OPENING: i16 = -15;
-pub static mut KING_ATTACKED_FIELDS_ENDING: i16 = -5;
+pub static mut KING_ATTACKED_FIELDS_OPENING: i16 = -16;
+pub static mut KING_ATTACKED_FIELDS_ENDING: i16 = 6;
 
