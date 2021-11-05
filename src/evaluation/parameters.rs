6b70ea7ba3c95549ddcae4435142cb32c5dc41a8
Tearth <tearthdev@gmail.com>
2021-11-05 20:17:32 +0100

    Adjust evaluation parameters

--- src/evaluation/parameters.rs
+++ src/evaluation/parameters.rs
@@ -1,6 +1,6 @@
-// ------------------------------------ //
-// Generated at 2021-11-02 20:29:37 UTC //
-// ------------------------------------ //
+// --------------------------------------------------- //
+// Generated at 2021-11-03 19:34:50 UTC (e = 0.064643) //
+// --------------------------------------------------- //
 
 pub static mut PIECE_VALUE: [i16; 6] = [95, 335, 375, 560, 1150, 10000];
 
@@ -8,22 +8,23 @@ pub static mut MOBILITY_OPENING: i16 = 2;
 pub static mut MOBILITY_ENDING: i16 = 3;
 
 pub static mut DOUBLED_PAWN_OPENING: i16 = 1;
-pub static mut DOUBLED_PAWN_ENDING: i16 = -9;
+pub static mut DOUBLED_PAWN_ENDING: i16 = -8;
 
-pub static mut ISOLATED_PAWN_OPENING: i16 = -22;
-pub static mut ISOLATED_PAWN_ENDING: i16 = 1;
+pub static mut ISOLATED_PAWN_OPENING: i16 = -21;
+pub static mut ISOLATED_PAWN_ENDING: i16 = 2;
 
-pub static mut CHAINED_PAWN_OPENING: i16 = 4;
-pub static mut CHAINED_PAWN_ENDING: i16 = 12;
+pub static mut CHAINED_PAWN_OPENING: i16 = 5;
+pub static mut CHAINED_PAWN_ENDING: i16 = 11;
 
-pub static mut PASSING_PAWN_OPENING: i16 = -7;
-pub static mut PASSING_PAWN_ENDING: i16 = 62;
+pub static mut PASSING_PAWN_OPENING: i16 = -5;
+pub static mut PASSING_PAWN_ENDING: i16 = 60;
 
-pub static mut PAWN_SHIELD_OPENING: i16 = 10;
-pub static mut PAWN_SHIELD_ENDING: i16 = 7;
+pub static mut PAWN_SHIELD_OPENING: i16 = 11;
+pub static mut PAWN_SHIELD_ENDING: i16 = 4;
 
-pub static mut PAWN_SHIELD_OPEN_FILE_OPENING: i16 = -21;
-pub static mut PAWN_SHIELD_OPEN_FILE_ENDING: i16 = 4;
+pub static mut PAWN_SHIELD_OPEN_FILE_OPENING: i16 = -23;
+pub static mut PAWN_SHIELD_OPEN_FILE_ENDING: i16 = 2;
 
-pub static mut KING_ATTACKED_FIELDS_OPENING: i16 = -16;
-pub static mut KING_ATTACKED_FIELDS_ENDING: i16 = 6;
+pub static mut KING_ATTACKED_FIELDS_OPENING: i16 = -15;
+pub static mut KING_ATTACKED_FIELDS_ENDING: i16 = 5;
+
