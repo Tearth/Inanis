41c5950f6eb1cdcab16f093bb9bb492b8dc61967
Tearth <tearthdev@gmail.com>
2021-11-07 13:39:55 +0100

    Move search to the separate thread

--- src/interface/uci.rs
+++ src/interface/uci.rs
@@ -5,35 +5,43 @@ use crate::engine::*;
 use crate::state::board::Bitboard;
 use crate::state::movescan::Move;
 use crate::state::*;
+use std::cell::UnsafeCell;
 use std::collections::HashMap;
 use std::io;
 use std::ops::Add;
 use std::process;
+use std::sync::Arc;
+use std::thread;
+use std::thread::JoinHandle;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
 
 struct UciState {
-    board: Bitboard,
-    options: HashMap<String, String>,
-    transposition_table: TranspositionTable,
-    pawn_hashtable: PawnHashTable,
+    board: UnsafeCell<Bitboard>,
+    options: UnsafeCell<HashMap<String, String>>,
+    transposition_table: UnsafeCell<TranspositionTable>,
+    pawn_hashtable: UnsafeCell<PawnHashTable>,
+    search_thread: UnsafeCell<Option<JoinHandle<()>>>,
 }
 
 impl Default for UciState {
     fn default() -> Self {
         UciState {
-            board: Bitboard::new_initial_position(),
-            options: HashMap::new(),
-            transposition_table: TranspositionTable::new(1 * 1024 * 1024),
-            pawn_hashtable: PawnHashTable::new(1 * 1024 * 1024),
+            board: UnsafeCell::new(Bitboard::new_initial_position()),
+            options: UnsafeCell::new(HashMap::new()),
+            transposition_table: UnsafeCell::new(TranspositionTable::new(1 * 1024 * 1024)),
+            pawn_hashtable: UnsafeCell::new(PawnHashTable::new(1 * 1024 * 1024)),
+            search_thread: UnsafeCell::new(None),
         }
     }
 }
 
+unsafe impl Sync for UciState {}
+
 pub fn run() {
-    let mut state: UciState = Default::default();
-    state.options.insert("Hash".to_string(), "1".to_string());
+    let mut state: Arc<UciState> = Arc::new(Default::default());
+    unsafe { (*state.options.get()).insert("Hash".to_string(), "1".to_string()) };
 
     println!("id name Inanis {}", VERSION);
     println!("id author {}", AUTHOR);
@@ -57,159 +65,166 @@ pub fn run() {
     }
 }
 
-fn handle_go(parameters: &[String], state: &mut UciState) {
-    let mut white_time = 1000;
-    let mut black_time = 1000;
-    let mut white_inc_time = 0;
-    let mut black_inc_time = 0;
-    let mut forced_depth = 0;
+fn handle_go(parameters: &[String], state: &mut Arc<UciState>) {
+    unsafe {
+        let mut white_time = 1000;
+        let mut black_time = 1000;
+        let mut white_inc_time = 0;
+        let mut black_inc_time = 0;
+        let mut forced_depth = 0;
 
-    let mut iter = parameters[1..].iter().peekable();
-    while let Some(token) = iter.next() {
-        match token.as_str() {
-            "wtime" => {
-                white_time = match iter.peek() {
-                    Some(value) => value.parse().unwrap_or(white_time),
-                    None => white_time,
+        let mut iter = parameters[1..].iter().peekable();
+        while let Some(token) = iter.next() {
+            match token.as_str() {
+                "wtime" => {
+                    white_time = match iter.peek() {
+                        Some(value) => value.parse().unwrap_or(white_time),
+                        None => white_time,
+                    }
                 }
-            }
-            "btime" => {
-                black_time = match iter.peek() {
-                    Some(value) => value.parse().unwrap_or(black_time),
-                    None => black_time,
+                "btime" => {
+                    black_time = match iter.peek() {
+                        Some(value) => value.parse().unwrap_or(black_time),
+                        None => black_time,
+                    }
                 }
-            }
-            "winc" => {
-                white_inc_time = match iter.peek() {
-                    Some(value) => value.parse().unwrap_or(white_inc_time),
-                    None => white_inc_time,
+                "winc" => {
+                    white_inc_time = match iter.peek() {
+                        Some(value) => value.parse().unwrap_or(white_inc_time),
+                        None => white_inc_time,
+                    }
                 }
-            }
-            "binc" => {
-                black_inc_time = match iter.peek() {
-                    Some(value) => value.parse().unwrap_or(black_inc_time),
-                    None => black_inc_time,
+                "binc" => {
+                    black_inc_time = match iter.peek() {
+                        Some(value) => value.parse().unwrap_or(black_inc_time),
+                        None => black_inc_time,
+                    }
                 }
-            }
-            "depth" => {
-                forced_depth = match iter.peek() {
-                    Some(value) => value.parse().unwrap_or(forced_depth),
-                    None => forced_depth,
+                "depth" => {
+                    forced_depth = match iter.peek() {
+                        Some(value) => value.parse().unwrap_or(forced_depth),
+                        None => forced_depth,
+                    }
                 }
+                _ => {}
             }
-            _ => {}
-        }
-    }
-
-    let time = match state.board.active_color {
-        WHITE => white_time,
-        BLACK => black_time,
-        _ => panic!("Invalid value: state.board.active_color={}", state.board.active_color),
-    };
-
-    let inc_time = match state.board.active_color {
-        WHITE => white_inc_time,
-        BLACK => black_inc_time,
-        _ => panic!("Invalid value: state.board.active_color={}", state.board.active_color),
-    };
-
-    let mut killers_table = Default::default();
-    let mut history_table = Default::default();
-
-    state.transposition_table.clear();
-    let context = SearchContext::new(
-        &mut state.board,
-        time,
-        inc_time,
-        forced_depth,
-        &mut state.transposition_table,
-        &mut state.pawn_hashtable,
-        &mut killers_table,
-        &mut history_table,
-    );
-
-    let mut best_move = Default::default();
-    for depth_result in context {
-        let mut output = String::new();
-        output = output.add(
-            &format!(
-                "info nodes {} depth {} time {} pv {}",
-                depth_result.statistics.nodes_count + depth_result.statistics.q_nodes_count,
-                depth_result.depth,
-                depth_result.time,
-                depth_result.best_move.to_text()
-            )
-            .to_string(),
-        );
-
-        if is_score_near_checkmate(depth_result.score) {
-            let mut moves_to_mate = (depth_result.score.abs() - CHECKMATE_SCORE).abs() / 2;
-            moves_to_mate *= depth_result.score.signum();
-
-            output = output.add(&format!(" score mate {}", moves_to_mate).to_string());
-        } else {
-            output = output.add(&format!(" score cp {}", depth_result.score).to_string());
         }
 
-        println!("{}", output);
+        let time = match (*state.board.get()).active_color {
+            WHITE => white_time,
+            BLACK => black_time,
+            _ => panic!("Invalid value: state.board.active_color={}", (*state.board.get()).active_color),
+        };
 
-        best_move = depth_result.best_move;
+        let inc_time = match (*state.board.get()).active_color {
+            WHITE => white_inc_time,
+            BLACK => black_inc_time,
+            _ => panic!("Invalid value: state.board.active_color={}", (*state.board.get()).active_color),
+        };
+
+        let state_arc = state.clone();
+        *state.search_thread.get() = Some(thread::spawn(move || {
+            let mut killers_table = Default::default();
+            let mut history_table = Default::default();
+
+            (*state_arc.transposition_table.get()).clear();
+            let context = SearchContext::new(
+                &mut *state_arc.board.get(),
+                time,
+                inc_time,
+                forced_depth,
+                &mut *state_arc.transposition_table.get(),
+                &mut *state_arc.pawn_hashtable.get(),
+                &mut killers_table,
+                &mut history_table,
+            );
+
+            let mut best_move = Default::default();
+            for depth_result in context {
+                let mut output = String::new();
+                output = output.add(
+                    &format!(
+                        "info nodes {} depth {} time {} pv {}",
+                        depth_result.statistics.nodes_count + depth_result.statistics.q_nodes_count,
+                        depth_result.depth,
+                        depth_result.time,
+                        depth_result.best_move.to_text()
+                    )
+                    .to_string(),
+                );
+
+                if is_score_near_checkmate(depth_result.score) {
+                    let mut moves_to_mate = (depth_result.score.abs() - CHECKMATE_SCORE).abs() / 2;
+                    moves_to_mate *= depth_result.score.signum();
+
+                    output = output.add(&format!(" score mate {}", moves_to_mate).to_string());
+                } else {
+                    output = output.add(&format!(" score cp {}", depth_result.score).to_string());
+                }
+
+                best_move = depth_result.best_move;
+                println!("{}", output);
+            }
+
+            println!("bestmove {}", best_move.to_text());
+        }));
     }
-
-    println!("bestmove {}", best_move.to_text());
 }
 
 fn handle_isready() {
     println!("readyok");
 }
 
-fn handle_position(parameters: &[String], state: &mut UciState) {
+fn handle_position(parameters: &[String], state: &mut Arc<UciState>) {
     if parameters.len() < 2 {
         return;
     }
 
-    state.board = match parameters[1].as_str() {
-        "fen" => {
-            let fen = parameters[2..].join(" ");
-            match Bitboard::new_from_fen(fen.as_str()) {
-                Ok(board) => board,
-                Err(message) => {
-                    println!("info string Error: {}", message);
-                    return;
+    unsafe {
+        *state.board.get() = match parameters[1].as_str() {
+            "fen" => {
+                let fen = parameters[2..].join(" ");
+                match Bitboard::new_from_fen(fen.as_str()) {
+                    Ok(board) => board,
+                    Err(message) => {
+                        println!("info string Error: {}", message);
+                        return;
+                    }
                 }
             }
-        }
-        _ => Bitboard::new_initial_position(),
-    };
+            _ => Bitboard::new_initial_position(),
+        };
+    }
 
     if let Some(index) = parameters.iter().position(|s| s == "moves") {
         for premade_move in &parameters[index + 1..] {
-            let parsed_move = match Move::from_text(premade_move, &state.board) {
+            let parsed_move = match Move::from_text(premade_move, unsafe { &mut *state.board.get() }) {
                 Ok(r#move) => r#move,
                 Err(message) => {
                     println!("info string Error: {}", message);
                     return;
                 }
             };
-            state.board.make_move(&parsed_move);
+            unsafe { (*state.board.get()).make_move(&parsed_move) };
         }
     };
 }
 
-fn handle_setoption(parameters: &[String], state: &mut UciState) {
+fn handle_setoption(parameters: &[String], state: &mut Arc<UciState>) {
     if parameters.len() < 2 {
         return;
     }
 
-    state.options.insert(parameters[2].to_string(), parameters[4].to_string());
+    unsafe { (*state.options.get()).insert(parameters[2].to_string(), parameters[4].to_string()) };
 }
 
-fn handle_ucinewgame(state: &mut UciState) {
-    let transposition_table_size = state.options["Hash"].parse::<usize>().unwrap() * 1024 * 1024;
-
-    state.board = Bitboard::new_initial_position();
-    state.transposition_table = TranspositionTable::new(transposition_table_size);
-    state.pawn_hashtable = PawnHashTable::new(1 * 1024 * 1024);
+fn handle_ucinewgame(state: &mut Arc<UciState>) {
+    unsafe {
+        let transposition_table_size = (*state.options.get())["Hash"].parse::<usize>().unwrap() * 1024 * 1024;
+        *state.board.get() = Bitboard::new_initial_position();
+        *state.transposition_table.get() = TranspositionTable::new(transposition_table_size);
+        *state.pawn_hashtable.get() = PawnHashTable::new(1 * 1024 * 1024);
+    }
 }
 
 fn handle_quit() {
