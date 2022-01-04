354bfc627687ab6697b92a8e3f22b953c7d353ef
Tearth <tearthdev@gmail.com>
2022-01-04 23:59:34 +0100

    Adjust time control

--- src/engine/clock.rs
+++ src/engine/clock.rs
@@ -2,6 +2,6 @@ pub fn get_time_for_move(total_time: u32, inc_time: u32, moves_to_go: u32) -> u3
     if moves_to_go == 0 {
         total_time / 30 + inc_time
     } else {
-        total_time / (moves_to_go + 2) + inc_time
+        total_time / (moves_to_go + 1) + inc_time
     }
 }
