e8c5ad553b8fe8740420d49e96b86f2915f85d93
Tearth <Tearth@users.noreply.github.com>
2022-03-18 12:26:50 +0000

    deploy: d22113d4c5abf519b37ad7c43ac8a72bd09eadd9

--- inanis/utils/benchmark/sidebar-items.js
+++ inanis/utils/benchmark/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["run",""]],"struct":[["BenchmarkResult",""]]});
+initSidebarItems({"fn":[["run","Runs a benchmark by performing a fixed-depth search for the built-in list of positions."]],"struct":[["BenchmarkResult",""]]});
