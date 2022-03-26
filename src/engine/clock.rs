fb4fe906db3b25f31a13a28c68717adf1260c583
Tearth <tearthdev@gmail.com>
2022-03-26 21:53:48 +0100

    Slightly increase margin in time allocator

--- src/engine/clock.rs
+++ src/engine/clock.rs
@@ -12,6 +12,6 @@ pub fn get_time_for_move(move_number: u16, total_time: u32, inc_time: u32, moves
 
         total_time / ((A + B * (C.min((move_number as f32) + D) / E).sin()) as u32) + inc_time
     } else {
-        total_time / (moves_to_go + 1) + inc_time
+        total_time / (moves_to_go + 2) + inc_time
     }
 }
