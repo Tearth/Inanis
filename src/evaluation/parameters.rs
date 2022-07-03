eca2e0fd2a2308f160f08064f98a734c0b3fea09
Tearth <tearthdev@gmail.com>
2022-07-03 21:00:56 +0200

    Adjust evaluation parameters

--- src/evaluation/parameters.rs
+++ src/evaluation/parameters.rs
@@ -1,5 +1,5 @@
 // --------------------------------------------------- //
-// Generated at 2022-05-13 18:52:01 UTC (e = 0.064056) //
+// Generated at 2022-07-03 13:23:55 UTC (e = 0.070760) //
 // --------------------------------------------------- //
 
 use super::*;
@@ -7,33 +7,33 @@ use super::*;
 impl Default for EvaluationParameters {
     fn default() -> Self {
         let mut evaluation_parameters = Self {
-            piece_value: [100, 420, 444, 650, 1326, 10000],
-            initial_material: 10308,
+            piece_value: [100, 381, 421, 632, 1262, 10000],
+            initial_material: 9860,
 
-            mobility_opening: [5, 8, 5, 6, 2, 6],
-            mobility_ending: [3, 0, 0, 5, 6, 3],
-            mobility_center_multiplier: [6, 1, 3, 1, 1, 6],
+            mobility_opening: [2, 4, 4, 4, 0, 2],
+            mobility_ending: [6, 2, 2, 5, 5, 5],
+            mobility_center_multiplier: [4, 3, 3, 2, 3, 4],
 
-            doubled_pawn_opening: 5,
-            doubled_pawn_ending: -12,
+            doubled_pawn_opening: 1,
+            doubled_pawn_ending: -14,
 
-            isolated_pawn_opening: -31,
-            isolated_pawn_ending: 1,
+            isolated_pawn_opening: -22,
+            isolated_pawn_ending: -8,
 
             chained_pawn_opening: 5,
-            chained_pawn_ending: 11,
+            chained_pawn_ending: 6,
 
-            passing_pawn_opening: -1,
-            passing_pawn_ending: 57,
+            passing_pawn_opening: -5,
+            passing_pawn_ending: 58,
 
-            pawn_shield_opening: 12,
-            pawn_shield_ending: 5,
+            pawn_shield_opening: 10,
+            pawn_shield_ending: 3,
 
-            pawn_shield_open_file_opening: -27,
-            pawn_shield_open_file_ending: 2,
+            pawn_shield_open_file_opening: -25,
+            pawn_shield_open_file_ending: 6,
 
-            king_attacked_fields_opening: -19,
-            king_attacked_fields_ending: 6,
+            king_attacked_fields_opening: -25,
+            king_attacked_fields_ending: 9,
 
             pst: [[[[0; 64]; 2]; 6]; 2],
             pst_patterns: [[[0; 64]; 2]; 6],
