f2400429c57e4696794bf756f6eb7c0bedb92158
Tearth <tearthdev@gmail.com>
2022-07-07 20:38:22 +0200

    Improve tunerset output

--- src/utils/tunerset.rs
+++ src/utils/tunerset.rs
@@ -12,6 +12,8 @@ use crate::state::movegen::MagicContainer;
 use crate::state::patterns::PatternsContainer;
 use crate::state::zobrist::ZobristContainer;
 use crate::utils::pgn::PGNLoader;
+use chrono::Utc;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -23,6 +25,7 @@ use std::sync::Arc;
 pub fn run(pgn_filename: &str, output_file: &str, min_ply: usize, max_score: i16, max_diff: u16, density: usize) {
     println!("Loading PGN file...");
 
+    let start_time = Utc::now();
     let file = match File::open(pgn_filename) {
         Ok(value) => value,
         Err(_) => {
@@ -32,7 +35,7 @@ pub fn run(pgn_filename: &str, output_file: &str, min_ply: usize, max_score: i16
     };
 
     let pgn_loader = PGNLoader::new(BufReader::new(file).lines());
-    let mut output_positions = Vec::new();
+    let mut output_positions = HashSet::new();
     let mut parsed_pgns = 0;
 
     let evaluation_parameters = Arc::new(EvaluationParameters::default());
@@ -47,6 +50,10 @@ pub fn run(pgn_filename: &str, output_file: &str, min_ply: usize, max_score: i16
     let history_table = Arc::new(HistoryTable::default());
     let abort_token = Arc::new(AtomicBool::new(false));
     let ponder_token = Arc::new(AtomicBool::new(false));
+
+    let mut total_viable_positions = 0;
+    let mut ignored_positions = 0;
+    let mut duplicates = 0;
 
     for pgn in pgn_loader {
         let pgn = match pgn {
@@ -93,41 +100,77 @@ pub fn run(pgn_filename: &str, output_file: &str, min_ply: usize, max_score: i16
             context.board.make_move(*r#move);
 
             if index < min_ply {
+                ignored_positions += 1;
+                continue;
+            }
+
+            if index > pgn.moves.len() - 12 {
+                ignored_positions += 1;
+                continue;
+            }
+
+            if index > 160 {
+                ignored_positions += 1;
                 continue;
             }
 
             if context.board.is_king_checked(context.board.active_color) {
+                ignored_positions += 1;
                 continue;
             }
 
             let q_score = qsearch::run::<false>(&mut context, 0, 0, MIN_ALPHA, MIN_BETA);
             if q_score > max_score {
+                ignored_positions += 1;
                 continue;
             }
 
             let evaluation = -((context.board.active_color as i16) * 2 - 1) * context.board.evaluate_without_cache();
             if evaluation.abs_diff(q_score) > max_diff {
+                ignored_positions += 1;
                 continue;
             }
 
             viable_positions.push(format!("{} c9 \"{}\";", context.board.to_epd(), pgn.result));
+            total_viable_positions += 1;
         }
 
-        for _ in 0..density {
+        let mut picked_positions = 0;
+        while picked_positions < density {
             if viable_positions.is_empty() {
                 break;
             }
 
             let index = fastrand::usize(0..viable_positions.len());
-            output_positions.push(viable_positions[index].to_owned());
+            let position = viable_positions[index].to_owned();
+
+            if output_positions.contains(&position) {
+                viable_positions.remove(index);
+                duplicates += 1;
+
+                continue;
+            }
+
+            output_positions.insert(viable_positions[index].to_owned());
             viable_positions.remove(index);
+            picked_positions += 1;
         }
 
         parsed_pgns += 1;
         if parsed_pgns % 1000 == 0 {
-            println!("Parsed PGNS: {} ({} output positions)", parsed_pgns, output_positions.len());
+            println!(
+                "Parsed PGNS: {} ({} viable positions, {} ignored positions, {} output positions, {} duplicates)",
+                parsed_pgns,
+                total_viable_positions,
+                ignored_positions,
+                output_positions.len(),
+                duplicates
+            );
         }
     }
+
+    println!("-----------------------------------------------------------------------------");
+    println!("Saving output...");
 
     let output_file = match File::create(output_file) {
         Ok(value) => value,
@@ -137,8 +180,12 @@ pub fn run(pgn_filename: &str, output_file: &str, min_ply: usize, max_score: i16
         }
     };
     let mut output_file_line_writer = LineWriter::new(output_file);
-
     for fen in output_positions {
         output_file_line_writer.write_all((fen + "\n").as_bytes()).unwrap();
     }
+
+    println!(
+        "Tuner dataset generation done in {:.2} s",
+        ((Utc::now() - start_time).num_milliseconds() as f32) / 1000.0
+    );
 }
