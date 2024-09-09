5828c5ad2e89038117a9583192aff0ebc0f4f4b7
Tearth <tearthdev@gmail.com>
2024-09-09 21:32:40 +0200

    Fix typo

--- src/evaluation/mobility.rs
+++ src/evaluation/mobility.rs
@@ -17,7 +17,6 @@ pub struct PieceMobility {
     pub outer: i8,
 }
 
-#[inline(never)]
 /// Evaluates mobility and part of the king safety on the `board` and returns score from the white color perspective (more than 0 when advantage,
 /// less than 0 when disadvantage). This evaluator does two things at once: first, counts all possible moves of knight, bishop, rook, queen
 /// (pawns and king are too slow and not very important), and second, sums how many squares around both kings are dangered by enemy side
